aa3874398fd869e63e66fc603f5d70875580cabe
HolyGrease <kireevdanil2000@gmail.com>
2021-09-12 19:52:44 +0300

    add: rest project

---
+++ tests/Matrix.Test/SingleThread.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Matrix.Test
+{
+    [TestClass]
+    public class SingleThread
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
