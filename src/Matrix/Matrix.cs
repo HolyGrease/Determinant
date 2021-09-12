9775c15b0d32ace7491484fbddb4595f86bd45d6
HolyGrease <kireevdanil2000@gmail.com>
2021-09-12 19:51:54 +0300

    add: default constructor

--- src/Matrix/Matrix.cs
+++ src/Matrix/Matrix.cs
@@ -4,6 +4,12 @@ namespace Matrix
 {
     public class Matrix
     {
+        private int[,] matrix;
+
+        public Matrix() {
+            matrix = new int[2,2] {{1, 0}, {0, 1}};
+        }
+
         public int Determinant() {
             return 0;
         }
