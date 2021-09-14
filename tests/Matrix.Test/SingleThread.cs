b9267668efcd344b48dc7648859b1386def3c36a
HolyGrease <kireevdanil2000@gmail.com>
2021-09-14 21:57:50 +0300

    add: some tests

--- tests/Matrix.Test/SingleThread.cs
+++ tests/Matrix.Test/SingleThread.cs
@@ -1,14 +1,93 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matrix;
 
 namespace Matrix.Test
 {
     [TestClass]
-    public class SingleThread
+    public class OneThread
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SimpleDimple()
         {
+            Matrix matrix = new Matrix();
 
+            int assert = 1;
+
+            int actual = matrix.Determinant();
+
+            Assert.AreEqual(assert, actual);
+        }
+
+        [TestMethod]
+        public void IdentityMatrixSize2()
+        {
+            Matrix matrix = new Matrix(new int[,] {{1, 0}, {0, 1}});
+
+            int assert = 1;
+
+            int actual = matrix.Determinant();
+
+            Assert.AreEqual(assert, actual);
+        }
+
+        [TestMethod]
+        public void IdentityMatrixSize3()
+        {
+            Matrix matrix = new Matrix(new int[,] {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
+
+            int assert = 1;
+
+            int actual = matrix.Determinant();
+
+            Assert.AreEqual(assert, actual);
+        }
+
+        [TestMethod]
+        public void IdentityMatrixSize4()
+        {
+            Matrix matrix = new Matrix(new int[,] {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}});
+
+            int assert = 1;
+
+            int actual = matrix.Determinant();
+
+            Assert.AreEqual(assert, actual);
+        }
+
+        [TestMethod]
+        public void MatrixSize2()
+        {
+            Matrix matrix = new Matrix(new int[,] {{1, 2}, {3, 4}});
+
+            int assert = -2;
+
+            int actual = matrix.Determinant();
+
+            Assert.AreEqual(assert, actual);
+        }
+
+        [TestMethod]
+        public void MatrixSize3()
+        {
+            Matrix matrix = new Matrix(new int[,] {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
+            int assert = 0;
+
+            int actual = matrix.Determinant();
+
+            Assert.AreEqual(assert, actual);
+        }
+
+        [TestMethod]
+        public void MatrixSize4()
+        {
+            Matrix matrix = new Matrix(new int[,] {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}});
+
+            int assert = 0;
+
+            int actual = matrix.Determinant();
+
+            Assert.AreEqual(assert, actual);
         }
     }
 }
